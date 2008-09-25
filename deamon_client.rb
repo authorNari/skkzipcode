2831c67539ef67408cf29d8c66f3e0bdb90be2f2
Narihiro Nakamura <authorNari@gmail.com>
2008-09-25 21:50:27 +0900

    add option. setting config of more client and more request.

--- deamon_client.rb
+++ deamon_client.rb
@@ -1,4 +1,17 @@
+#!/usr/bin/ruby -wKU
+
 require 'socket'
+require 'optparse'
+require 'benchmark'
+
+@client = 1
+@request = 1500
+@client = 100 if $DEBUG
+@request = 1000 if $DEBUG
+opt = OptionParser.new
+opt.on("-c CNT", "--client CNT", Integer) {|c| @client = c}
+opt.on("-r CNT", "--request CNT", Integer) {|c| @request = c}
+opt.parse!(ARGV)
 
 def input_zipcodes(filepath)
   codes = []
@@ -10,18 +23,29 @@ def input_zipcodes(filepath)
     end
   end
   res = []
-  1000.times { res << codes[(rand * codes.size).to_i] }
+  @request.times { res << codes[(rand * codes.size).to_i] }
   res
 end
 
 list = input_zipcodes(File.join(File.dirname(__FILE__), "SKK-JISYO.zipcode"))
 list = ARGV unless ARGV.size.zero?
 
-list.each do |e|
-  s = TCPSocket.new("localhost", 12345)
-  s.puts(e)
-  while s.gets
-    puts $_
+@client.times do
+  fork do
+    Benchmark.bm(7, "request/sec:") do |x|
+      t = x.report("request_time:") do
+        list.each do |e|
+          s = TCPSocket.new("localhost", 12345)
+          s.puts(e)
+          while s.gets
+            puts $_
+          end
+          s.close
+        end
+      end
+      [t/@request]
+    end
+    exit!
   end
-  s.close
 end
+
